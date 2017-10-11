ef144dd96ab75bef22b1859a42e819c85769b88c
Reuben Kandiah <yes@kandoo.io>
2017-10-11 16:50:15 +0100

    Variable Declaration & Call to summariseR function

--- hackDay/summariseR_variables.r
+++ hackDay/summariseR_variables.r
@@ -1,4 +1,4 @@
-# Declare Variables to pass to the summariseR script
+# Declare Variables to pass to the summariseR function
 # In this instance, we are passing an Irish Times Article with 2 paragraphs limit on the summary
 
 arg1 <- 'https://www.irishtimes.com/1.3249486'
