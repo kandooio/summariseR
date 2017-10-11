41d674e66da86afdeeb100a0d9c29f9b9340a2e5
Reuben Kandiah <yes@kandoo.io>
2017-10-11 16:24:22 +0100

    Create summariseR_variables.r

---
+++ hackDay/summariseR_variables.r
@@ -0,0 +1,6 @@
+# Declare Variables to pass to the summariseR script
+# In this instance, we are passing an Irish Times Article with 2 paragraphs limit on the summary
+
+arg1 <- 'https://www.irishtimes.com/1.3249486'
+arg2 <- 2
+source('../summariseR.r')
