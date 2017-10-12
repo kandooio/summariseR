501fcff2c700620f3f4eea463ebf6d10ae262a0a
Reuben Kandiah <yes@kandoo.io>
2017-10-12 10:15:55 +0100

    Delete summariseR_variables.r

--- hackDay/summariseR_variables.r
+++
@@ -1,7 +0,0 @@
-# Declare Variables to pass to the summariseR function
-# In this instance, we are passing an Irish Times Article with 2 paragraphs limit on the summary
-
-arg1 <- 'https://www.irishtimes.com/1.3249486'
-arg2 <- 2
-summariseR_function <- dget("summariseR_source.r")
-summariseR_function(arg1,arg2)
